6ee7a039cf5631215941e13854fe9b35cfa941a8
antoniojr100 <95947435+antoniojr100@users.noreply.github.com>
2025-01-02 16:44:01 -0500

    test

--- src/Art.js
+++ src/Art.js
@@ -1,6 +1,6 @@
 import './App.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEnvelope, faMusic, faAppleAlt, faAppleWhole, faHeadphones, faGuitar, faSpotify } from '@fortawesome/free-solid-svg-icons'
+import { faEnvelope, faMusic, faAppleAlt, faAppleWhole, faHeadphones, faGuitar, faSpotify, faVideo } from '@fortawesome/free-solid-svg-icons'
 // import {} from '@fortawesome/'
 
 function Art(){
@@ -9,7 +9,13 @@ function Art(){
             <div className="wrapper">
             <h2 class="sub-header">My Music</h2>
                 
-            <h3 className="mary-elizabeth">Mary Elizabeth</h3>
+            <h3 className="music-title">Mary Elizabeth</h3>
+
+                    {/* width: 560 height: 315 */}
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/BW_-sO0QCCI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+
+
+
             <ul className="music-links" id="mary-elizabeth">
                 <li className="link-container">
                     <a href="https://open.spotify.com/artist/2t1Y3bw5KerpUeejGVQu2T"
@@ -18,7 +24,7 @@ function Art(){
                     >
                         <FontAwesomeIcon icon={faHeadphones} className="icon" />
                     </a>
-                    <h3>Spotify</h3>
+                    <h4 className="icon-title">Spotify</h4>
                 </li>
                 <li className="link-container">
                     <a href="https://maryelizabeth1.bandcamp.com/"
@@ -27,7 +33,16 @@ function Art(){
                     >
                         <FontAwesomeIcon icon={faGuitar} className="icon" />
                     </a>
-                    <h3>Bandcamp</h3>
+                    <h4 className="icon-title">Bandcamp</h4>
+                </li>
+                <li className="link-container">
+                    <a href="https://www.youtube.com/channel/UC7hW9jGNdHxBXLqQk90Sj3A"
+                        target='_blank'
+                        rel="noreferrer"
+                    >
+                        <FontAwesomeIcon icon={faVideo} className="icon" />
+                    </a>
+                    <h4 className="icon-title">YouTube</h4>
                 </li>
             </ul>
 
@@ -35,7 +50,7 @@ function Art(){
                         Weight
                         Alteration 
                 */}
-                <h3 className="artist-name">Subtle Nosebleed</h3>
+                <h3 className="music-title">Subtle Nosebleed</h3>
                 <ul className="music-links" id="subtle-nosebleed">
                     <li className="link-container">
                         <a href="https://open.spotify.com/artist/2YUsSdjo4pEJHq81JGQHWR?si=Vd0OPpQARUG9uzQMrkJZfA&nd=1"
@@ -44,7 +59,7 @@ function Art(){
                         >
                             <FontAwesomeIcon icon={faHeadphones} className="icon" />
                         </a>
-                        <h4>Spotify</h4>
+                        <h4 className="icon-title">Spotify</h4>
                     </li>
                     <li className="link-container">
                         <a href="https://music.apple.com/ca/artist/subtle-nosebleed/1495282605"
@@ -53,7 +68,7 @@ function Art(){
                         >
                             <FontAwesomeIcon icon={faMusic} className="icon" />
                         </a>
-                        <h4>Apple Music</h4>
+                        <h4 className="icon-title">Apple Music</h4>
                     </li>
                 </ul>
 
