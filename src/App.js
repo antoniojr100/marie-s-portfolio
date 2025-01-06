b267d089a68218448ff89e4373009593493e3eb6
antoniojr100 <95947435+antoniojr100@users.noreply.github.com>
2025-01-06 08:37:58 -0500

    removed unused items

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import Header from './Header.js';
-import About from './About.js';
+// import About from './About.js';
 import Art from './Art.js';
-import Contact from './Contact.js';
+// import Contact from './Contact.js';
 import Footer from './Footer.js';
-import {useState} from 'react';
+// import {useState} from 'react';
 
 
 function App() {
--- src/Art.js
+++ src/Art.js
@@ -1,6 +1,6 @@
 import './App.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEnvelope, faMusic, faAppleAlt, faAppleWhole, faHeadphones, faGuitar, faSpotify, faVideo } from '@fortawesome/free-solid-svg-icons'
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { faEnvelope, faMusic, faAppleAlt, faAppleWhole, faHeadphones, faGuitar, faSpotify, faVideo } from '@fortawesome/free-solid-svg-icons'
 // import {} from '@fortawesome/'
 
 function Art(){
@@ -35,67 +35,6 @@ function Art(){
                         <iframe src="https://open.spotify.com/embed/album/1IZzpTadHOuYwzRMJDA4tu?utm_source=generator" width="560" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy" title="Spotify"></iframe>
                     </div>
                 </div>
-
-                    {/* width: 560 height: 315 */}
-            {/* <iframe width="560" height="315" src="https://www.youtube.com/embed/BW_-sO0QCCI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe> */}
-
-
-
-            {/* <ul className="music-links" id="mary-elizabeth">
-                <li className="link-container">
-                    <a href="https://open.spotify.com/artist/2t1Y3bw5KerpUeejGVQu2T"
-                        target='_blank'
-                        rel="noreferrer"
-                    >
-                        <FontAwesomeIcon icon={faHeadphones} className="icon" />
-                    </a>
-                    <h4 className="icon-title">Spotify</h4>
-                </li>
-                <li className="link-container">
-                    <a href="https://maryelizabeth1.bandcamp.com/"
-                        target='_blank'
-                        rel="noreferrer"
-                    >
-                        <FontAwesomeIcon icon={faGuitar} className="icon" />
-                    </a>
-                    <h4 className="icon-title">Bandcamp</h4>
-                </li>
-                <li className="link-container">
-                    <a href="https://www.youtube.com/channel/UC7hW9jGNdHxBXLqQk90Sj3A"
-                        target='_blank'
-                        rel="noreferrer"
-                    >
-                        <FontAwesomeIcon icon={faVideo} className="icon" />
-                    </a>
-                    <h4 className="icon-title">YouTube</h4>
-                </li>
-            </ul> */}
-
-                {/* Subtle Nosebleed
-                        Weight
-                        Alteration 
-                */}
-                {/* <h3 className="music-title">Subtle Nosebleed</h3>
-                <ul className="music-links" id="subtle-nosebleed">
-                    <li className="link-container">
-                        <a href="https://open.spotify.com/artist/2YUsSdjo4pEJHq81JGQHWR?si=Vd0OPpQARUG9uzQMrkJZfA&nd=1"
-                            target='_blank'
-                            rel="noreferrer"
-                        >
-                            <FontAwesomeIcon icon={faHeadphones} className="icon" />
-                        </a>
-                        <h4 className="icon-title">Spotify</h4>
-                    </li>
-                    <li className="link-container">
-                        <a href="https://music.apple.com/ca/artist/subtle-nosebleed/1495282605"
-                            target='_blank'
-                            rel="noreferrer"
-                        >
-                            <FontAwesomeIcon icon={faMusic} className="icon" />
-                        </a>
-                        <h4 className="icon-title">Apple Music</h4>
-                    </li>
-                </ul> */}
             </div>
         </section>
     );
